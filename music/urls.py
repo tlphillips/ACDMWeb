c0c57b2ac8c832c0fa3f2deabcfaa55bc7a4b7be
tlphillips <trex1964@gmail.com>
2018-05-05 11:09:40 -0400

    Update

--- music/urls.py
+++ music/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
 
     # /music/71/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
