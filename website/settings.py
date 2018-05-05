45ca9980f9029bd1758b8c7ef285752137342c70
tlphillips <trex1964@gmail.com>
2018-05-05 09:34:52 -0400

    Update

--- website/settings.py
+++ website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['165.227.201.231']
 # Application definition
 
 INSTALLED_APPS = [
+    'music.apps.MusicCongig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
