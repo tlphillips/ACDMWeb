0ee2e2a86b011657401c0d10ec35490c6cb1af35
tlphillips <trex1964@gmail.com>
2018-05-05 10:41:30 -0400

    Update

--- music/views.py
+++ music/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
+from .models import Album
+from django.template import loader
+import django
+
 
 def index(request):
-    return HttpResponse("<h1>This is Music</h1>")
+    all_albums = Album.objects.all()
+    template = loader.get_template('/music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+
+    return HttpResponse(template.render(request, context))
+
+
+def detail(requst, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
