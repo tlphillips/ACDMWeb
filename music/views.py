e84a358fc53884e931bdf2cdad688d0dd7267a4e
tlphillips <trex1964@gmail.com>
2018-05-05 21:05:21 -0400

    Update

--- music/views.py
+++ music/views.py
@@ -6,9 +6,7 @@ from django.http import Http404
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
